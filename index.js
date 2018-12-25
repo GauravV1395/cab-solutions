2c3327167e6b9f2d0480a021b2116450612e41a6
Gaurav V <gaurav.v1395@gmail.com>
2018-12-25 18:28:31 +0530

    updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { mongoose } = require('./config/db');
 const { routes } = require('./config/routes');
 const cors = require('cors');
+ socket = require('socket.io');
 const port = 3001;
 
 app.use(express.json());
@@ -15,3 +16,16 @@ app.listen(port, () => {
     console.log('listening to port', port);
 });
 
+server = app.listen(8080, function(){
+    console.log('server is running on port 8080')
+});
+
+io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('SEND_MESSAGE', function(data){
+        io.emit('RECEIVE_MESSAGE', data);
+    })
+});
